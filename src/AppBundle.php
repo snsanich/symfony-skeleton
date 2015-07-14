3824c5330a9a1c94fda30faf709242017eaa2001
Sergey Protko <fesors@gmail.com>
2015-07-14 10:34:45 +0300

    Allow to register services from bundle configuration

--- src/AppBundle.php
+++ src/AppBundle.php
@@ -2,8 +2,21 @@
 
 namespace App;
 
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AppBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $loader = new YamlFileLoader($container, new FileLocator([
+            __DIR__ . '/Resources/config'
+        ]));
+        $loader->load('services.yml');
+    }
+
 }
